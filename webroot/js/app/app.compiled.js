14c29a920b95fe52696a3ce7cd87510a4606e193
Mitsunori Inaba <m-inaba@phoenixdesign.jp>
2014-07-15 09:57:12 +0900

    minify追加

--- webroot/js/app/app.compiled.js
+++ webroot/js/app/app.compiled.js
@@ -106,6 +106,9 @@ System.register("util", [], function() {
       }
     });
   }
+  function uuid(a) {
+    return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuid);
+  }
   return {
     get trace_func() {
       return trace_func;
@@ -121,6 +124,9 @@ System.register("util", [], function() {
     },
     get myDialog() {
       return myDialog;
+    },
+    get uuid() {
+      return uuid;
     }
   };
 });
